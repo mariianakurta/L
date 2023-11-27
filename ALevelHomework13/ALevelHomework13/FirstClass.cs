e22a297b75dee2a71b8c7c5df39354d8e3eedba7
Mariana <hotpot.hotpot.hotpot22@gmail.com>
2023-11-27 18:45:57 +0100

    My thirteenth homework commit.

---
+++ ALevelHomework13/ALevelHomework13/FirstClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALevelHomework13
+{
+    internal class FirstClass
+    {
+        public delegate void MainDelegate(bool result);
+
+        public MainDelegate Show { get; set; }
+
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ ALevelHomework13/ALevelHomework13/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ALevelHomework13;
+
+public class Program
+{
+    public static void DisplayResult(bool result)
+    {
+        Console.WriteLine($"Result: {result}");
+    }
+
+    public static void DisplayListResult(List<bool> listsResult)
+    {
+        Console.WriteLine("Results List:");
+        foreach (var result in listsResult)
+        {
+            Console.WriteLine($" - {result}");
+        }
+    }
+
+    public static void Main()
+    {
+        FirstClass firstClass = new FirstClass();
+        SecondClass secondClass = new SecondClass();
+
+        firstClass.Show = DisplayResult;
+
+        SecondClass.DelegatesResult delegatesFirstResult = secondClass.Calc(firstClass.Show, 1, 2);
+        SecondClass.DelegatesResult delegatesSecondResult = secondClass.Calc(firstClass.Show, 3, 4);
+
+        bool displayingFirstResult = delegatesFirstResult(2);
+        bool displayingSecondResult = delegatesSecondResult(4);
+
+        firstClass.Show(displayingFirstResult);
+        firstClass.Show(displayingSecondResult);
+
+        DisplayListResult(secondClass.ListResult);
+    }
+}
