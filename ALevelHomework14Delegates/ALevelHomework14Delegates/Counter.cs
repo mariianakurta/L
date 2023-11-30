cc39be4fec7e73774532a8a61b189019ae2af740
Mariana <hotpot.hotpot.hotpot22@gmail.com>
2023-11-30 23:49:39 +0100

    My fourteenth homework commit.

---
+++ ALevelHomework14Delegates/ALevelHomework14Delegates/Counter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALevelHomework14Delegates
+{
+    public class Counter
+    {
+        public event EventHandler<MyEventArgs> CalsulationsResult;
+
+        public void Count(int x, int y, MainDelegate totalCount)
+        {
+            try
+            {
+                int result = totalCount(x, y);
+                CorrectOrException($"Result: {result}");
+            }
+            catch (Exception exception)
+            {
+                CorrectOrException($"Exception: {exception.Message}");
+            }
+        }
+
+        void CorrectOrException(string message)
+        {
+            CalsulationsResult?.Invoke(this, new MyEventArgs(message));
+        }
+    }
+}
---
+++ ALevelHomework14Delegates/ALevelHomework14Delegates/EventDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALevelHomework14Delegates
+{
+
+    public delegate int MainDelegate(int x, int y);
+
+
+    public class MyEventArgs : EventArgs
+    {
+        public string EventData { get; set; }
+
+        public MyEventArgs(string data)
+        {
+            EventData = data;
+        }
+    }
+}
