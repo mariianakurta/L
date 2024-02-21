c9744a51c5bfd390882b28bf8e09dc789901f262
Mariana <hotpot.hotpot.hotpot22@gmail.com>
2024-02-22 00:29:35 +0100

    My twenty-ninth homework commit.

---
+++ eShop-Sample2/Catalog/Catalog.Host/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Catalog.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm",
+            "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("products")]
+        public IActionResult GetProducts()
+        {
+            var products = new[]
+            {
+                "Product1", "Product2", "Product3", "Product4", "Product5"
+            };
+
+            return Ok(products);
+        }
+    }
+}
